f32a5a741d76590128be90bd5aa6201d5702eaa0
ajiit23 <ajitinfo143@gmail.com>
2025-06-26 01:31:40 +0530

    index

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,19 +2,18 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+
 import path from "path";
+
 import { connectDB } from "./lib/db.js";
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-import { setupSocket } from "./lib/socket.js";
-import { createServer } from "http";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const app = express();
-const server = createServer(app);
-
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
 app.use(express.json());
@@ -31,13 +30,11 @@ app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
-
-// Setup socket.io
-setupSocket(server);
 
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
