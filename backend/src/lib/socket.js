8f272777d371032a27e1487f223d0a905e9a8e55
ajiit23 <ajitinfo143@gmail.com>
2025-06-26 01:29:24 +0530

    socket

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -5,34 +5,16 @@ import express from "express";
 const app = express();
 const server = http.createServer(app);
 
-export function setupSocket(server) {
-  const io = new Server(server, {
-    cors: {
-      origin: ["http://localhost:5173"],
-    },
-  });
-
-  const userSocketMap = {};
-
-  io.on("connection", (socket) => {
-    console.log("A user connected", socket.id);
-
-    const userId = socket.handshake.query.userId;
-    if (userId) userSocketMap[userId] = socket.id;
-
-    io.emit("getOnlineUsers", Object.keys(userSocketMap));
-
-    socket.on("disconnect", () => {
-      console.log("A user disconnected", socket.id);
-      delete userSocketMap[userId];
-      io.emit("getOnlineUsers", Object.keys(userSocketMap));
-    });
-  });
-}
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:5173"],
+  },
+});
 
 export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
 }
+
 
 const userSocketMap = {}; // {userId: socketId}
 
